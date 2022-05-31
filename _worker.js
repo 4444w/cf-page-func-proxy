044b5588a6b969a9f3dc9c3c2a415fec293ce36b
4444w <63961296+4444w@users.noreply.github.com>
2022-05-31 13:23:18 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -2,7 +2,7 @@ export default {
     async fetch(request, env) {
       let url = new URL(request.url);
       if (url.pathname.startsWith('/')) {
-        url.hostname="h4444w.herokuapp.com";
+        url.hostname="81.71.51.6:5244";
         let new_request=new Request(url,request);
         return fetch(new_request);
       }
